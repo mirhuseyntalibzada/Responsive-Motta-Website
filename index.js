17ab1a297ea6c510ccb26f420faaabbe7e904487
Mirhuseyn Talibzada <126802509+mirhuseyntalibzada@users.noreply.github.com>
2024-01-04 07:07:40 -0800

    Add files via upload

---
+++ index.js
@@ -0,0 +1,22 @@
+const slider = document.querySelector('.slider');
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener('mousedown', (e) => {
+    isDown = true
+    startX = e.pageX - slider.offsetLeft;
+    scrollLeft = slider.scrollLeft;
+});
+
+slider.addEventListener('mouseup', () => {
+    isDown = false
+});
+
+slider.addEventListener('mousemove', (e) => {
+    if(!isDown) return;
+    e.preventDefault();
+    const x = e.pageX - slider.offsetLeft; 
+    const walk = (x- startX);
+    slider.scrollLeft = scrollLeft - walk;
+});
